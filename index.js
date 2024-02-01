8d2993c1254e5904ad597b8279a2d9d5a3bfdf45
salskh95 <salskh95@gmail.com>
2024-02-01 15:26:10 +0400

    updated rendervideos function in js and updated alternate rows styling in css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let playlistId = "";
 const apiKey = "AIzaSyCKHytj5BTTR324N9R4NXnud41v1vhYwiw";
 const table = document.getElementById("dynamicTable");
 const videoCountElement = document.querySelector(".videoCount");
+let isPrivateOrDeleted = false;
 videoCountElement.style.display = "none";
 
 function fetchData(url) {
@@ -14,133 +15,23 @@ function fetchData(url) {
     });
 }
 
-function renderVideos(videos) {
-  const dynamicTable = document.getElementById("dynamicTable");
-  removeChildExceptClass(dynamicTable, "header-row");
-  videos.forEach((video, index) => {
-    const newRow = document.createElement("div");
-    newRow.classList.add("table-row");
+function formatDateTime(dateTimeString) {
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+  const date = new Date(dateTimeString);
+  return date.toLocaleString("en-US", options);
+}
 
-    if (index % 2 === 1) {
-      newRow.classList.add("alternate-row");
-    }
-
-    const numberCell = document.createElement("div");
-    numberCell.classList.add("table-cell", "table-number", "dynamic-cell");
-    numberCell.textContent = index + 1;
-    newRow.appendChild(numberCell);
-
-    const titleCell = document.createElement("div");
-    titleCell.classList.add("table-cell", "dynamic-cell", "table-title");
-    titleCell.textContent = video.snippet.title;
-
-    if (
-      video?.snippet?.title === "Private video" ||
-      video?.snippet?.title === "Deleted video"
-    ) {
-      newRow.classList.add("unavailable-video");
-    }
-
-    newRow.appendChild(titleCell);
-
-    const descriptionCell = document.createElement("div");
-    descriptionCell.classList.add("table-cell", "dynamic-cell");
-
-    if (
-      video.snippet.description.length > 0 &&
-      !(
-        video?.snippet?.title === "Private video" ||
-        video?.snippet?.title === "Deleted video"
-      )
-    ) {
-      const descriptionText = document.createElement("div");
-      descriptionText.classList.add("truncated-text");
-      descriptionText.textContent = truncateText(video.snippet.description, 50);
-      descriptionCell.appendChild(descriptionText);
-
-      const showMoreLink = document.createElement("a");
-      showMoreLink.textContent = "Show more";
-      showMoreLink.href = "#";
-      showMoreLink.classList.add("show-more-link");
-
-      showMoreLink.addEventListener("click", function (e) {
-        e.preventDefault();
-        if (descriptionText.classList.contains("truncated-text")) {
-          descriptionText.classList.remove("truncated-text");
-          descriptionText.classList.add("expanded-text");
-          descriptionText.textContent = video.snippet.description;
-          showMoreLink.textContent = "Show less";
-        } else {
-          descriptionText.classList.remove("expanded-text");
-          descriptionText.classList.add("truncated-text");
-          descriptionText.textContent = truncateText(
-            video.snippet.description,
-            50
-          );
-          showMoreLink.textContent = "Show more";
-        }
-      });
-
-      if (video.snippet.description.length > 50) {
-        showMoreLink.style.display = "inline";
-      } else {
-        showMoreLink.style.display = "none";
-      }
-
-      descriptionCell.appendChild(showMoreLink);
-    }
-
-    newRow.appendChild(descriptionCell);
-
-    const thumbnailCell = document.createElement("div");
-    thumbnailCell.classList.add("table-cell");
-    thumbnailCell.innerHTML = `<a href="https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}" target="_blank"><img src=${video.snippet.thumbnails?.medium?.url} /></a>`;
-    newRow.appendChild(thumbnailCell);
-
-    const dateCell = document.createElement("div");
-    dateCell.classList.add("table-cell", "dynamic-cell", "table-date");
-    dateCell.textContent = formatDateTime(video.snippet.publishedAt);
-    newRow.appendChild(dateCell);
-
-    const publishDateCell = document.createElement("div");
-    publishDateCell.classList.add("table-cell", "dynamic-cell", "table-date");
-
-    if (
-      video.contentDetails &&
-      video.contentDetails.videoPublishedAt &&
-      new Date(video.contentDetails.videoPublishedAt).toString() !==
-        "Invalid Date"
-    ) {
-      publishDateCell.textContent = formatDateTime(
-        video.contentDetails.videoPublishedAt
-      );
-    } else {
-      publishDateCell.textContent = "";
-    }
-
-    newRow.appendChild(publishDateCell);
-
-    table.appendChild(newRow);
-
-    function formatDateTime(dateTimeString) {
-      const options = {
-        hour: "2-digit",
-        minute: "2-digit",
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-      };
-      const date = new Date(dateTimeString);
-      return date.toLocaleString("en-US", options);
-    }
-
-    function truncateText(text, maxLength) {
-      if (text.length > maxLength) {
-        return text.slice(0, maxLength) + "...";
-      }
-      return text;
-    }
-  });
+function truncateText(text, maxLength) {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + "...";
+  }
+  return text;
 }
 
 function removeChildExceptClass(parent, className) {
@@ -150,6 +41,59 @@ function removeChildExceptClass(parent, className) {
       parent.removeChild(child);
     }
   }
+}
+
+function renderVideos(videos) {
+  const dynamicTable = document.getElementById("dynamicTable");
+  removeChildExceptClass(dynamicTable, "header-row");
+
+  const rowsContainer = document.createElement("div");
+
+  videos.forEach((video, index) => {
+    isPrivateOrDeleted =
+      video?.snippet?.title === "Private video" ||
+      video?.snippet?.title === "Deleted video";
+
+    const newRow = document.createElement("div");
+    newRow.classList.add("table-row");
+
+    newRow.classList.toggle("unavailable-video", isPrivateOrDeleted);
+
+    newRow.innerHTML = `
+    <div class="table-cell table-number dynamic-cell">${index + 1}</div>
+    <div class="table-cell dynamic-cell table-title">${
+      video.snippet.title
+    }</div>
+    <div class="table-cell dynamic-cell">
+    ${
+      video.snippet.description.length > 0 && !isPrivateOrDeleted
+        ? `<div class="truncated-text">${truncateText(
+            video.snippet.description,
+            50
+          )}</div>
+        <a href="#" class="show-more-link">Show more</a>`
+        : ""
+    }
+    </div>
+    <div class="table-cell">
+    <a href="https://www.youtube.com/watch?v=${
+      video.snippet.resourceId.videoId
+    }" target="_blank">
+    <img src=${video.snippet.thumbnails?.medium?.url} />
+    </a>
+    </div>
+    <div class="table-cell dynamic-cell table-date">${formatDateTime(
+      video.snippet.publishedAt
+    )}</div>
+      <div class="table-cell dynamic-cell table-date">${
+        video.contentDetails?.videoPublishedAt
+          ? formatDateTime(video.contentDetails.videoPublishedAt)
+          : ""
+      }</div>
+      `;
+    rowsContainer.appendChild(newRow);
+  });
+  dynamicTable.appendChild(rowsContainer);
 }
 
 function searchPlaylist() {
